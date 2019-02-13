5c5ca656b6885d3ef90f5b1bad6081e27e594364
Christopher Joseph Jr <christopher.joseph.jr@gmail.com>
2019-02-13 19:23:16 +0000

    Revert "AI-3.3 <Chris J@DESKTOP-5866BFJ Merge branch 'master'"
    This reverts commit 792d07a6a7283c84f335f508a6f6b67a057c4d0f

---
+++ app/src/main/java/edu/odu/cs/air411/wherearfthou/MainActivity.java
@@ -0,0 +1,68 @@
+package edu.odu.cs.air411.wherearfthou;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Button button2;
+    private Button button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openFoundReportActivity();
+            }
+
+        });
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openLostReportActivity();
+            }
+        });
+
+        button3 = findViewById(R.id.map);
+        button3.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openReportMapActivity();
+            }
+
+        });
+    }
+    private void openFoundReportActivity()
+    {
+        Intent intent = new Intent(MainActivity.this, FoundReportActivity.class);
+        startActivity(intent);
+    }
+    private void openLostReportActivity()
+    {
+        Intent intent = new Intent(MainActivity.this, LostReportActivity.class);
+        startActivity(intent);
+    }
+    private void openReportMapActivity()
+    {
+        Intent intent = new Intent(MainActivity.this, LostReportActivity.class);
+        startActivity(intent);
+    }
+}
+
