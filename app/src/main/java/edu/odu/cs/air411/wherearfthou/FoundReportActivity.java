df424864dcf9401dc7df070e4d2a5b105a5a5388
Tony Hoang <ton01554@gmail.com>
2019-02-20 14:22:58 -0500

    Changing titles to found and lost pages, setting parents in Manifest

--- app/src/main/java/edu/odu/cs/air411/wherearfthou/FoundReportActivity.java
+++ app/src/main/java/edu/odu/cs/air411/wherearfthou/FoundReportActivity.java
@@ -1,8 +1,11 @@
 package edu.odu.cs.air411.wherearfthou;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class FoundReportActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class FoundReportActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_found_report);
     }
+
+    /**
+    public void submitForm(View view){
+        Intent submit = new Intent();
+
+        //EditText descriptText = findViewById<EditText>(R.id.descriptText);
+        //String description = descriptText.getText().toString();
+    }
+     */
 
 }
