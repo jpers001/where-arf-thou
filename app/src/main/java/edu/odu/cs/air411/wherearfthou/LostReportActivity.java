867e6bbfae115f8fb4d5e39bf84dfbe79b2f7f54
Tony Hoang <ton01554@gmail.com>
2019-02-25 11:42:44 -0500

    implementing some basic functionality for LostReportActivity as well as further implementation of ReportData

--- app/src/main/java/edu/odu/cs/air411/wherearfthou/LostReportActivity.java
+++ app/src/main/java/edu/odu/cs/air411/wherearfthou/LostReportActivity.java
@@ -1,15 +1,64 @@
 package edu.odu.cs.air411.wherearfthou;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class LostReportActivity extends AppCompatActivity {
+
+    public ArrayList<ReportData> report = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost_report);
+
+        Button submitBtn2 = findViewById(R.id.submitBtn2);
+        submitBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitForm(report);
+            }
+        });
+
+        ImageButton addPhotoImgBtn = findViewById(R.id.addPhotoImgBtn);
+        addPhotoImgBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent openTakePhotoActivity = new Intent(LostReportActivity.this, TakePhotoActivity.class);
+                startActivity(openTakePhotoActivity);
+            }
+        });
+    }
+
+
+
+    public void submitForm(ArrayList<ReportData> report){
+        Intent submit = new Intent(LostReportActivity.this, MainActivity.class);
+
+        EditText nameEditText = findViewById(R.id.editText);
+        String name = nameEditText.getText().toString();
+
+        EditText descriptEditText2 = findViewById(R.id.descriptEditText2);
+        String description = descriptEditText2.getText().toString();
+
+        EditText lastSeenEditText = findViewById(R.id.editText2);
+        String lastSeen = lastSeenEditText.getText().toString();
+
+        EditText contactEditText = findViewById(R.id.editText3);
+        String contact = contactEditText.getText().toString();
+
+        ReportData entry = new ReportData(name, description, lastSeen, contact, false);
+        report.add(entry);
+
+        startActivity(submit);
     }
 
 }
