75c2e8b8019955cc0bf3b8079c40ab58ed417677
jpersaud <jpers001@odu.edu>
2019-03-27 14:17:26 -0400

    Updating main layout and changing background picture.

--- app/src/main/java/edu/odu/cs/air411/wherearfthou/MainActivity.java
+++ app/src/main/java/edu/odu/cs/air411/wherearfthou/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.odu.cs.air411.wherearfthou;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,9 +14,17 @@ public class MainActivity extends AppCompatActivity {
     private Button button3;
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
+
+
+
     {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
