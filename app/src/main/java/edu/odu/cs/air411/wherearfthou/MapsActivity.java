25b5a1181a6ba07cdfbcd9daddaf4dc1cf0aa840
jpersaud <jpers001@odu.edu>
2019-04-15 14:18:38 -0400

    Merge branch 'new_main' of C:\Users\jpersaud\StudioProjects\where-arf-thou-GITIGNor with conflicts.

--- app/src/main/java/edu/odu/cs/air411/wherearfthou/MapsActivity.java
+++ app/src/main/java/edu/odu/cs/air411/wherearfthou/MapsActivity.java
@@ -14,6 +14,7 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.widget.Toast;
